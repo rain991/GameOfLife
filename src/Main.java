527f265509d78ff851bb7a27325b0683a90bc60f
IvanSavenko <savenkoivan931@gmail.com>
2024-03-22 11:05:32 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // Projekt zrobil Ivan Savenko  (last changes - 22.03)
 public class Main {
-    public static void main(String[] args) { // w main'e mamy glowne parametry gry
+    public static void main(String[] args) { // w main'ie mamy glowne parametry gry
         int width = 10;
         int height = 10;
         int iterations = 10;
